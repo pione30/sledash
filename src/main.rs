037cec1bb8db35dab0f9aec5fd92fa6903b026a9
pione30 <g.pionelish30@gmail.com>
2020-03-08 20:26:06 +0900

    GET request to emoji.list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{header, Client};
+use std::collections::HashMap;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let token = env::var("SLACK_APP_ACCESS_TOKEN")?;
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::AUTHORIZATION,
+        header::HeaderValue::from_str(format!("Bearer {}", token).as_str())?,
+    );
+
+    let client = Client::builder().default_headers(headers).build()?;
+
+    let response = client
+        .get("https://slack.com/api/emoji.list")
+        .send()
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    println!("{:#?}", response);
+    Ok(())
 }
