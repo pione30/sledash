aa5c7098bf40d06577a84bd88fb4d6a5abd32f80
pione30 <g.pionelish30@gmail.com>
2020-03-15 03:15:33 +0900

    fix `has already been defined in this module` error and suppress warnings

--- magickwand-bindgen/build.rs
+++ magickwand-bindgen/build.rs
@@ -1,5 +1,20 @@
+use std::collections::HashSet;
 use std::env;
 use std::path::PathBuf;
+
+// https://github.com/rust-lang/rust-bindgen/issues/687#issuecomment-450750547
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
@@ -10,10 +25,28 @@ fn main() {
         .probe("MagickWand")
         .expect("pkg-config MagickWand should be probed");
 
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INT_UPWARD".into(),
+            "FP_INT_DOWNWARD".into(),
+            "FP_INT_TOWARDZERO".into(),
+            "FP_INT_TONEARESTFROMZERO".into(),
+            "FP_INT_TONEAREST".into(),
+            "FP_NAN".into(),
+            "FP_INFINITE".into(),
+            "FP_ZERO".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "IPPORT_RESERVED".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
     let mut bindgen_builder = bindgen::Builder::default()
         .header("wrapper.h")
         .rustfmt_bindings(true)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+        .parse_callbacks(Box::new(ignored_macros));
 
     for include_path in &wand_config.include_paths {
         bindgen_builder = bindgen_builder.clang_arg(format!("-I{}", include_path.display()));
