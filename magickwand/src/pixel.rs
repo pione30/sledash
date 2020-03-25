bc83a08fb3efad92e91f46f87fa8cdb3bdb5b819
pione30 <g.pionelish30@gmail.com>
2020-03-26 01:52:51 +0900

    add Pixel

---
+++ magickwand/src/pixel.rs
@@ -0,0 +1,52 @@
+use magickwand_bindgen;
+use magickwand_bindgen::MagickBooleanType_MagickFalse as MagickFalse;
+// use magickwand_bindgen::MagickBooleanType_MagickTrue as MagickTrue;
+
+use std::ffi::CString;
+
+use crate::error;
+
+pub struct Pixel {
+    ptr: *mut magickwand_bindgen::PixelWand,
+}
+
+impl Pixel {
+    pub fn new() -> Self {
+        let ptr = unsafe { magickwand_bindgen::NewPixelWand() };
+        Pixel { ptr }
+    }
+
+    pub fn clear_pixel_wand(&self) {
+        unsafe { magickwand_bindgen::ClearPixelWand(self.ptr) };
+    }
+
+    pub fn pixel_set_color(&self, color: &str) -> Result<(), error::ExceptionType> {
+        let c_color = CString::new(color).expect("CString::new color");
+        let status = unsafe { magickwand_bindgen::PixelSetColor(self.ptr, c_color.as_ptr()) };
+
+        if status == MagickFalse {
+            Err(self.pixel_get_exception_type())
+        } else {
+            Ok(())
+        }
+    }
+
+    fn pixel_get_exception_type(&self) -> error::ExceptionType {
+        let exception_type = unsafe { magickwand_bindgen::PixelGetExceptionType(self.ptr) };
+        error::get_exception_type(exception_type)
+    }
+}
+
+impl Default for Pixel {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Drop for Pixel {
+    fn drop(&mut self) {
+        unsafe {
+            self.ptr = magickwand_bindgen::DestroyPixelWand(self.ptr);
+        }
+    }
+}
