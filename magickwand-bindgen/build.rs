72f1db41677fa4d0cf0fec2dcae9ba35fb135fa2
pione30 <g.pionelish30@gmail.com>
2020-03-29 14:23:03 +0900

    atleast_version 7.0

--- magickwand-bindgen/build.rs
+++ magickwand-bindgen/build.rs
@@ -21,7 +21,7 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
     let wand_config = pkg_config::Config::new()
-        .atleast_version("6.9")
+        .atleast_version("7.0")
         .probe("MagickWand")
         .expect("pkg-config MagickWand should be probed");
 
