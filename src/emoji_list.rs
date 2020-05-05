5785ad9f4855d95fc2a872dd66b4f8f959368ba6
pione30 <g.pionelish30@gmail.com>
2020-05-05 17:11:33 +0900

    change emoji_list::fetch to return Result

--- src/emoji_list.rs
+++ src/emoji_list.rs
@@ -9,24 +9,24 @@ pub struct EmojiListResponse {
     pub error: Option<String>,
 }
 
-pub async fn fetch(token: &str) -> EmojiListResponse {
+pub async fn fetch(token: &str) -> Result<EmojiListResponse, String> {
     let mut headers = header::HeaderMap::new();
     headers.insert(
         header::AUTHORIZATION,
         header::HeaderValue::from_str(format!("Bearer {}", token).as_str())
-            .expect("Bearer token should be a valid header value"),
+            .map_err(|error| format!("Invalid token was passed: {}", error))?,
     );
 
     let client = Client::builder()
         .default_headers(headers)
         .build()
-        .expect("Client should be built");
+        .map_err(|error| format!("reqwest client couldn't be built: {}", error))?;
 
     let response = client
         .get("https://slack.com/api/emoji.list")
         .send()
         .await
-        .unwrap_or_else(|error| {
+        .map_err(|error| {
             let url = error.url().map_or_else(
                 || "URL not found.".to_string(),
                 |url| url.as_str().to_owned(),
@@ -37,11 +37,13 @@ pub async fn fetch(token: &str) -> EmojiListResponse {
                 |status| status.as_str().to_owned(),
             );
 
-            panic!("API response error: URL: {}, HTTP status: {}", url, status);
-        });
+            format!("API response error: URL: {}, HTTP status: {}", url, status)
+        })?;
 
-    response
-        .json::<EmojiListResponse>()
-        .await
-        .expect("The response body should be in JSON format or be properly deserialized")
+    response.json::<EmojiListResponse>().await.map_err(|error| {
+        format!(
+            "the response body is not in JSON format or it cannot be properly deserialized: {}",
+            error
+        )
+    })
 }
